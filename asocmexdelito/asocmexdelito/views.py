3384a782a87d7fcb478377fabef885a003d96470
mario <memunoz083@gmail.com>
2021-05-14 17:03:04 -0500

    Primer Subida

---
+++ asocmexdelito/asocmexdelito/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse
+import datetime
+from django.template import Template,Context
+
+
+def saludo(request):
+    doc_saludo=open("C:/Asociacion Mexicana Contra Delitos Ciberneticos/asocmexdelito/asocmexdelito/templates/asocmexdelito/index.html")
+    plt=Template(doc_saludo.read())
+    doc_saludo.close()
+    contexto=Context()
+
+    documento=plt.render(contexto)
+    return HttpResponse(documento)
+
+
+    return HttpResponse()
+
+def despedida(request):
+    return HttpResponse("Que te vaya bien amigo")
+
+def fechaactual(request):
+
+    fecha_actual=datetime.datetime.now()
+    return HttpResponse(fecha_actual)
+
+def calculaEdad(request,anio):
+    edadActual=22
+    periodo=anio-2020
+    edadFutura=edadActual+periodo
+    documento="<html><body><h2>En el año %s tendrás %s </body></html>" %(anio,edadFutura)
+    return HttpResponse(documento)
