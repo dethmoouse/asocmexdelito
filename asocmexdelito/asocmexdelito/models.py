6488efc31235dfaa2505b2e9ba519dc8f631f597
mario <memunoz083@gmail.com>
2021-05-17 15:38:02 -0500

    clase datos_participantes

--- asocmexdelito/asocmexdelito/models.py
+++ asocmexdelito/asocmexdelito/models.py
@@ -1,5 +1,28 @@
 from django.db import models
 
+CREATE TABLE datos_participantes(
+    "id_participante" serial NOT NULL PRIMARY KEY,
+    "nombre" varchar(30) NOT NULL
+);
+
+
+class datos_participantes(models.Model):
+    id_participante = models.IntegerField(default=0,primary_key=true)
+    nombre = models.CharField(max_length=50)
+    a_paterno = models.CharField(max_length=20)
+    a_materno =models.CharField(max_length=20)
+    sexo = models.CharField(max_length=1)
+    edad = models.IntegerField()
+    telefono = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=80)
+    email = models.CharField(max_length=40)
+    status = models.CharField(max_length=40)
+    motivo = models.CharField(max_length=120)
+    form_academica = models.CharField(max_length=50)
+    semestre_egresado = models.CharField(max_length=8)
+
+
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
