3a50bd6749eeb7f921d19741a5ccb82db30602d4
mario <memunoz083@gmail.com>
2021-05-17 15:39:35 -0500

    clase datos_participantes, llamada de datos definidos

--- asocmexdelito/asocmexdelito/views.py
+++ asocmexdelito/asocmexdelito/views.py
@@ -2,24 +2,25 @@ from django.http import HttpResponse
 import datetime
 from django.template import Template,Context
 
+fecha=datetime.datetime.now()
+
+class datos_participantes(object):
+    def __init__(self, id_participante, nombre, a_paterno, a_materno):
+
+        self.id_participante=id_participante
+        self.nombre=nombre
+        self.a_paterno=a_paterno
+        self.a_materno=a_materno
 
 
 def index(request):
-    fecha=datetime.datetime.now()
+
     doc_index=open("C:/Asociacion Mexicana Contra Delitos Ciberneticos/asocmexdelito/asocmexdelito/templates/asocmexdelito/index.html")
     plt=Template(doc_index.read())
     doc_index.close()
     contexto=Context({"fecha_consulta":fecha})
     documento=plt.render(contexto)
     return HttpResponse(documento)
-
-
-    return HttpResponse()
-
-def fechaactual(request):
-
-    fecha_actual=datetime.datetime.now()
-    return HttpResponse(fecha_actual)
 
 def calculaEdad(request,anio):
     edadActual=22
@@ -29,10 +30,11 @@ def calculaEdad(request,anio):
     return HttpResponse(documento)
 
 def registro(request):
+    participante=datos_participantes("1","Mario","muñoz","falcon")
     doc_registro=open("C:/Asociacion Mexicana Contra Delitos Ciberneticos/asocmexdelito/asocmexdelito/templates/asocmexdelito/registro.html")
     construct_reg=Template(doc_registro.read())
     doc_registro.close()
-    contexto=Context()
+    contexto=Context({"nombre_participante":participante.nombre,"a_paterno":participante.a_paterno,"id_participante":participante.id_participante})
     documento=construct_reg.render(contexto)
     return HttpResponse(documento)
 
@@ -40,6 +42,6 @@ def consulta(request):
     doc_consulta=open("C:/Asociacion Mexicana Contra Delitos Ciberneticos/asocmexdelito/asocmexdelito/templates/asocmexdelito/consulta.html")
     construct_consulta=Template(doc_consulta.read())
     doc_consulta.close()
-    contexto=Context()
+    contexto=Context({"fecha_consulta":fecha})
     documento=construct_consulta.render(contexto)
     return HttpResponse(documento)
